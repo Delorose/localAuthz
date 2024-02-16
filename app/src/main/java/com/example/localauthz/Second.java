8d7173dd4535ef8863965d00a5b97249908d6655
Delorose <ligendadulus@gmail.com>
2024-02-16 18:01:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/localauthz/Second.java
@@ -0,0 +1,16 @@
+package com.example.localauthz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+import com.example.localauthz.R;
+
+
+public class Second extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
